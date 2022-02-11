cd183eb3a35cae67400d4e59332221855277b23c
olwar <olliairola@gmail.com>
2022-02-11 13:53:44 +0200

    done

--- ft_print.c
+++ ft_print.c
@@ -21,52 +21,6 @@ static const converter *my_array[12] =
 	ft_putdecimal
 };
 
-/* int	massive_if(char c, va_list args, int i)
-{
-	if (c == '%s')
-		va_arg(args, char *)
-		putstr
-	else if (c == '%c')
-		va_arg(args, char)
-		putchar
-	else if (c == '%d')
-		va_arg(args, int)
-		putnbr
-	else if (c == '%f')
-		va_arg(args, float)
-		putfloat
-	else if (c == '%x')
-		va_arg(args, int)
-		puthexa
-	else if (c == '%u')
-		va_arg(args, unsigned int)
-		putunsigned
-	else if (c == '%p')
-		va_arg(args, void *)
-		putpointer
-	else if (c == '%%' || '\%')
-		putchar('%')
-	else if (c == '%o')
-		va_arg(args, int)
-		putoctal
-	else if (c == '%f')
-		va_arg(args, float)
-	else if (c == '%0')
-		va_arg(args, int)
-		putzerofill
-	else if (c == '%.')
-		va_arg(args, float)
-		putdecimal
-	else if (c == '\n' || c == '\t' || c == '\b' || c == '\f' || c == '\r' || \
-	c == '\v' || c == '\\' || c == '\n' || )
-		putchar(c)
-	else
-		putstr(error)
-
-} */
-
-/* make flag 0-4 index, fieldt_with 5-... etc... */
-
 void	add_info(t_list *pr)
 {
 	if (pr->array)
@@ -74,7 +28,7 @@ void	add_info(t_list *pr)
 		pr->temp = ft_intdup(pr->array, sizeof(pr->array) / sizeof(int));
 		free(pr->array);
 	}
-	pr->array = ft_intdup(pr->temp, sizeof(pr->temp) / sizeof(int) + sizeof(int));
+	pr->array = ft_append(pr->temp, pr->i, sizeof(pr->temp) / sizeof(int) + sizeof(int));
 }
 
 void	checker(t_list *pr, char c)
@@ -83,20 +37,44 @@ void	checker(t_list *pr, char c)
 	{
 		if (pr->flag[pr->i] == c)
 			add_info(&*pr);
+		pr->i++;
 	}
+	while (pr->field_width[pr->i])
+	{
+		if (pr->field_width[pr->i] == c)
+			add_info(&*pr);
+		pr->i++;
+	}
+	if (pr->precision[pr->i] == c)
+		add_info(&*pr);
+	pr->i++;
+	while (pr->length_modifier[pr->i])
+	{
+		if (pr->length_modifier[pr->i] == c)
+			add_info(&*pr);
+		pr->i++;
+	}
+	while (pr->conversion_specifier[pr->i])
+	{
+		if (pr->conversion_specifier[pr->i] == c)
+			add_info(&*pr);
+		pr->i++;
+	}
+	if (pr->percent[pr->i] == c)
+		add_info(&*pr);
 }
 
-void	initializer(char c)
+void	initializer(char c, va_list args, int *i)
 {
 	t_list pr;
-	
+	/* take care that c is not z */
 	pr.i = 0;
 	pr.flag = "#0- +";
-	pr.field_width = "123456789*";
-	pr.precision = ".";
-	pr.length_modifier = "hl"; /*hh, h, l, ll*/
-	pr.conversion_specifier = "diouxX";
-	pr.percent = "%";
+	pr.field_width = "zzzzz123456789*";
+	pr.precision = "zzzzzzzzzzzzzzz.";
+	pr.length_modifier = "zzzzzzzzzzzzzzzhhllhl"; /*hh, h, l, ll*/
+	pr.conversion_specifier = "zzzzzzzzzzzzzzzzzzzzzzdiouxX";
+	pr.percent = "zzzzzzzzzzzzzzzzzzzzzzzzzzzzz%";
 	checker(&pr, c);
 }
 
@@ -104,14 +82,19 @@ int	ft_printf(const char *format, ...)
 {
 	int	i;
 	va_list args;
+	char *special_string_part
 
 	i = 0;
 	va_start(args, format);
 	while (format[i] != '\0')
 	{
 		if (format[i] == '%')
-			initializer(format[i + 1], args, i);
-		write(1, format[i], 1);
+		{	/* make i skip special character */
+			special_string_part = initializer(format[i + 1], args, &i); 
+			special_printer_for_my_special_baby(special_string_part);
+		}
+		else
+			write(1, format[i], 1);
 	}
 	va_end(args);
 }
